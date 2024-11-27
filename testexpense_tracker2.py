c18e2b38e689551847fe4d49b1d36d0288de9eb0
ledetom12 <lkidane1@terpmail.umd.edu>
2024-11-26 20:42:37 -0500

    Update testexpense_tracker2.py

--- testexpense_tracker2.py
+++ testexpense_tracker2.py
@@ -14,3 +14,11 @@ class TestBudgetLimit:
         budget.set_budget(1500, "Housing")
         assert budget.budget_limit["Housing"] == 1500
         assert budget.totalallocated == 1500
+    def test_adjustments(self):
+        budget = exp.BudgetLimit(total_budget=4000, threshold= 0.8)
+        budget.set_budget(2000, "Housing")
+        expected_ratio = 3200/4000
+        adjustments = budget.adjustments()
+        
+        assert adjustments is None
+        assert adjustments["Housing"] == int(2000 * expected_ratio *100) /100
